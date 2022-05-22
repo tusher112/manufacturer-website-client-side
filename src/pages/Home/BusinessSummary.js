4126c299b37e442a0da68449618243eef9e80504
tusher112 <mdtusherahmed820@gmail.com>
2022-05-22 15:19:16 +0600

    business summary  added

--- src/pages/Home/BusinessSummary.js
+++ src/pages/Home/BusinessSummary.js
@@ -2,9 +2,42 @@ import React from 'react';
 
 const BusinessSummary = () => {
     return (
-        <div>
-        <p>Business summary</p>
+        <div class="my-5  shadow flex justify-center text-center grid cols-1 md:grid-cols-3 lg:grid-cols-3 ">
+  <div class="stat">
+    <div class="stat-figure text-primary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+    </div>
+    <div class="stat-title">Total Products sold</div>
+    <div class="stat-value text-primary">25.6K</div>
+    <div class="stat-desc">21% sell increase in the last month</div>
+  </div>
+  
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+    </div>
+    <div class="stat-title">Ship on Time</div>
+    <div class="stat-value text-secondary">97%</div>
+    <div class="stat-desc">99% on time shipping last month</div>
+  </div>
+  
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <div class="avatar online">
+        <div class="w-16 rounded-full">
+          <img src="https://api.lorem.space/image/face?w=128&h=128" />
         </div>
+      </div>
+    </div>
+    <div class="stat-value">86%</div>
+    <div class="stat-title">Positive Seller Ratings
+</div>
+    <div class="stat-desc text-secondary"></div>
+  </div>
+  
+
+  
+</div>
     );
 };
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
+import BusinessSummary from './BusinessSummary';
 import Footer from './Footer';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <BusinessSummary></BusinessSummary>
             <Footer></Footer>
         </div>
     );
